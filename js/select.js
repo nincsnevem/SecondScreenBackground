4af601cff5098a9220918f1cc1e8003a589c8d36
ninnev <kosarnagy.mark@gmail.com>
2026-02-24 17:59:09 +0100

    added circle gradient option

--- js/select.js
+++ js/select.js
@@ -16,10 +16,17 @@ animDVDButton.addEventListener("click", () => setVisible("dvd"));
 const snakeButton = document.getElementById("game-anim-snake");
 snakeButton.addEventListener("click", () => setVisible("snake"));
 
+const simpleOffButton = document.getElementById("simple-anim-off");
+simpleOffButton.addEventListener("click", () => setVisible("off"));
+
+const simpleCircleButton = document.getElementById("simple-anim-circle");
+simpleCircleButton.addEventListener("click", () => setVisible("circle"));
+
 const digitalClock = document.getElementById("digitalClockContainer");
 const analogClock = document.getElementById("analogClockContainer");
 const dvdLogo = document.getElementById("dvdLogo");
 const snakeContainer = document.getElementById("snakeContainer");
+const circleGradient = document.getElementById("gradientContainer");
 
 
 
@@ -43,17 +50,21 @@ function setVisible(status) {
     snakeContainer.style.setProperty("visibility", "hidden");
     snakeContainer.style.setProperty("opacity", "0");
 
+    circleGradient.style.setProperty("visibility", "hidden");
+    circleGradient.style.setProperty("opacity", "0");
+
     document.querySelectorAll(".active").forEach(el => {
         el.classList.remove("active");
     });
 
     offButton.classList.add("active");
     animOffButton.classList.add("active");
+    simpleOffButton.classList.add("active");
 
 
     if (status == "digital") {
         document.querySelectorAll(".clockOption.active").forEach(el => {
-        el.classList.remove("active");
+            el.classList.remove("active");
         });
 
         digitalButton.classList.add("active");
@@ -64,7 +75,7 @@ function setVisible(status) {
     }
     if (status == "analog") {
         document.querySelectorAll(".clockOption.active").forEach(el => {
-        el.classList.remove("active");
+            el.classList.remove("active");
         });
         analogButton.classList.add("active");
         analogClock.style.setProperty("visibility", "visible");
@@ -75,7 +86,7 @@ function setVisible(status) {
 
     if (status == "dvd") {
         document.querySelectorAll(".game-anim-option.active").forEach(el => {
-        el.classList.remove("active");
+            el.classList.remove("active");
         });
 
 
@@ -87,14 +98,22 @@ function setVisible(status) {
     }
 
     if (status == "snake") {
-        
+
         document.querySelectorAll(".game-anim-option.active").forEach(el => {
-        el.classList.remove("active");
+            el.classList.remove("active");
         });
         snakeButton.classList.add("active");
 
         snakeContainer.style.setProperty("visibility", "visible");
         snakeContainer.style.setProperty("opacity", "1");
+    }
+    if (status === "circle") {
+        document.querySelectorAll(".simple-anim-option.active").forEach(el => {
+            el.classList.remove("active");
+        });
+        simpleCircleButton.classList.add("active");
+        circleGradient.style.setProperty("visibility", "visible");
+        circleGradient.style.setProperty("opacity", "1");
     }
     localStorage.setItem("Status", status);
 }
